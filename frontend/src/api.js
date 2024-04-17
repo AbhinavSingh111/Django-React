a65f7b1aff57889ac823286450dd27d61621a54f
Abhinav Singh <singh111abhinav@gmail.com>
2024-04-17 22:00:59 +0530

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 import {ACCESS_TOKEN} from './constants'
 
+const apiUrl = 'https://1aa7037c-6222-464b-b933-b79af3d80ee0-dev.e1-us-cdp-2.choreoapis.dev/django-react/backend/rest-api-be2/v1.0'
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL:apiUrl
 })
 
 api.interceptors.request.use(
